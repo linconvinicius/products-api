8b9b1fcc725bfe3c3dab49b060206be70ad935c9
linconvinicius <lvini_@live.com>
2024-12-10 15:28:30 -0300

    add dto, controller and repository

---
+++ src/main/java/com/productmanager/productapi/dtos/ProductRecordDto.java
@@ -0,0 +1,9 @@
+package com.productmanager.productapi.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+}
---
+++ src/main/java/com/productmanager/productapi/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.productmanager.productapi.repositories;
+
+import com.productmanager.productapi.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
